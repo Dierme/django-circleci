1ae7c3dd457a2e2f79c79aec8eaeeb6d9204c999
Oleksii Prykhodko <oleksii@cetrez.com>
2020-08-20 20:51:59 +0200

    Normalize application structure. Add DB volume mounted on the host. Use named volume for static files.

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -13,5 +13,6 @@ fi
 
 python manage.py flush --no-input
 python manage.py migrate
+python manage.py collectstatic --clear --no-input
 gunicorn djangoci.wsgi:application --bind 0.0.0.0:8000
 #exec "$@"
