83321f3facf909cfc185ccfe34cbfbf1bbb68fc3
Oleksii Prykhodko <oleksii@cetrez.com>
2020-10-14 23:10:45 +0200

    add healthcheck route

--- app/djangoci/views.py
+++ app/djangoci/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
 def index(request):
     return HttpResponse(f"Hello world from Django! You're at the index.")
+
+
+def healthcheck(request):
+    return JsonResponse({"success": True})
