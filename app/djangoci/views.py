6130a396310c3e88d6e2d71ea985265493def93e
Oleksii Prykhodko <oleksii@cetrez.com>
2021-02-19 18:05:43 +0100

    Testing Circleci

--- app/djangoci/views.py
+++ app/djangoci/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, JsonResponse
 
 
 def index(request):
-    return HttpResponse(f"Hello world from Django! You're at the index.")
+    return HttpResponse(f"Hello world from Django! You're at the index. Bingo")
 
 
 def healthcheck(request):
